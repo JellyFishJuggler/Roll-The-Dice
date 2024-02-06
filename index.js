b0a7f6a2e0e5812e2339e4d5fe19e2a4ee56d561
Srijan Anand Gupta <146509602+JellyFishJuggler@users.noreply.github.com>
2024-02-06 20:21:50 +0530

    Files Commited

---
+++ index.js
@@ -0,0 +1,11 @@
+var randomNumber = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage = "dice" + randomNumber + ".png";
+var randomImageSource = "assets/" + randomDiceImage;
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "assets/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
