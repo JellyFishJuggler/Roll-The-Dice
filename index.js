0f4f818a4e606e412de393036aea0e4ec6bd18e5
Srijan Anand Gupta <guptasrijan85@gmail.com>
2024-03-09 21:51:53 +0530

    Updates

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 var randomImageSource2 = "assets/dice" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+function reloadPage(){
+    location.reload();
+}
